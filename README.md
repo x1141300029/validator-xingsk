ce60ed7f7fed3b93a3ff42d668f0fa84bb6b204d
xingsk <1141300029@qq.com>
2020-09-11 16:36:33 +0800

    /**  * isArray:是否为数组  * isChinese:是否为中文  * isDouble:是否为浮点数(小数)  * isIDCard:是否为身份证号  * isInteger:是否为整数  * isLetter:是否为全英文  * isNull:是否为空  * isNumber:是否为数字  * isObject:是否为json数据  * isPhone:是否为手机号  * isZip:是否为邮编  */

--- README.md
+++ README.md
@@ -9,3 +9,27 @@ qq:1141300029@qq.com
 ```
 npm install validator-xingsk -D
 ```
+###### 使用
+```
+import {isPhone} from 'validator-xingsk'
+
+console.log(isPhone('13161497787')) //true
+console.log(isPhone('131')) //false
+```
+###### 可用函数
+```
+/**
+ * isArray:是否为数组
+ * isChinese:是否为中文
+ * isDouble:是否为浮点数(小数)
+ * isIDCard:是否为身份证号
+ * isInteger:是否为整数
+ * isLetter:是否为全英文
+ * isNull:是否为空
+ * isNumber:是否为数字
+ * isObject:是否为json数据
+ * isPhone:是否为手机号
+ * isZip:是否为邮编
+ */
+```
+
--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 /**
  * isArray:是否为数组
+ * isChinese:是否为中文
+ * isDouble:是否为浮点数(小数)
+ * isIDCard:是否为身份证号
+ * isInteger:是否为整数
+ * isLetter:是否为全英文
  * isNull:是否为空
- * isObject:是否为json
+ * isNumber:是否为数字
+ * isObject:是否为json数据
  * isPhone:是否为手机号
- * isEmail:是否为邮箱
+ * isZip:是否为邮编
  */
 module.exports={
   isArray:require('./lib/isArray'),
+  isChinese:require('./lib/isChinese'),
+  isDouble:require('./lib/isDouble'),
+  isEmail:require('./lib/isEmail'),
+  isIDCard:require('./lib/isIDCard'),
+  isInteger:require('./lib/isInteger'),
+  isLetter:require('./lib/isLetter'),
   isNull:require('./lib/isNull'),
+  isNumber:require('./lib/isNumber'),
   isObject:require('./lib/isObject'),
   isPhone:require('./lib/isPhone'),
-  isEmail:require('./lib/isEmail')
+  isZip:require('./lib/isZip'),
 };
