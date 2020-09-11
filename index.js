b0fe333bb92b475396042ba5073b3cd21ecc14e9
xingsk <1141300029@qq.com>
2020-09-11 16:26:15 +0800

    首次提交

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * isArray:是否为数组
+ * isNull:是否为空
+ * isObject:是否为json
+ * isPhone:是否为手机号
+ * isEmail:是否为邮箱
+ */
+module.exports={
+  isArray:require('./lib/isArray'),
+  isNull:require('./lib/isNull'),
+  isObject:require('./lib/isObject'),
+  isPhone:require('./lib/isPhone'),
+  isEmail:require('./lib/isEmail')
+};
